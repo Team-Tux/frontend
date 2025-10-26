e9097ce5fc77bdb69d79dd231d650f35a9d4953b
Jonas <mail@jonasgoebel.net>
2025-10-26 06:47:08 +0100

    merge fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,11 @@
 import React, { Suspense, useEffect } from "react";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { CSpinner, useColorModes } from "@coreui/react";
-import "./scss/style.scss";
 
 import { CSpinner, useColorModes } from '@coreui/react'
 import './scss/style.scss'
 
 // We use those styles to show code examples, you should remove them in your application.
-import './scss/examples.scss'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import axios from 'axios'
 
@@ -17,7 +14,6 @@ const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
 
 // Pages
 const Login = React.lazy(() => import('./views/pages/login/Login'))
-const Register = React.lazy(() => import('./views/pages/register/Register'))
 const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
 
@@ -51,7 +47,6 @@ const App = () => {
         >
           <Routes>
             <Route exact path="/login" name="Login Page" element={<Login />} />
-            <Route exact path="/register" name="Register Page" element={<Register />} />
             <Route exact path="/404" name="Page 404" element={<Page404 />} />
             <Route exact path="/500" name="Page 500" element={<Page500 />} />
             <Route path="*" name="Home" element={<DefaultLayout />} />
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -8,10 +8,6 @@ const incidents = React.lazy(() => import('./views/incidents/Incidents'))
 const CreateIncident = React.lazy(() => import('./views/incidents/CreateIncident'))
 const IncidentDetails = React.lazy(() => import('./views/incidents/IncidentDetails'))
 
-const Colors = React.lazy(() => import("./views/theme/colors/Colors"));
-const Typography = React.lazy(
-  () => import("./views/theme/typography/Typography"),
-);
 
 const routes = [
   { path: "/", exact: true, name: "Home" },
@@ -21,28 +17,6 @@ const routes = [
   { path: '/incidents/create', name: 'Create Incident', element: CreateIncident },
   { path: '/incidents/:id', name: 'Incident Details', element: IncidentDetails },
   { path: '/helper', name: 'Helper', element: helper },
-  { path: "/theme", name: "Theme", element: Colors, exact: true },
-  { path: "/theme/colors", name: "Colors", element: Colors },
-  { path: "/theme/typography", name: "Typography", element: Typography },
-  { path: "/base", name: "Base", element: Cards, exact: true },
-  { path: "/base/accordion", name: "Accordion", element: Accordion },
-  { path: "/base/breadcrumbs", name: "Breadcrumbs", element: Breadcrumbs },
-  { path: "/base/cards", name: "Cards", element: Cards },
-  { path: "/base/carousels", name: "Carousel", element: Carousels },
-  { path: "/base/collapses", name: "Collapse", element: Collapses },
-  { path: "/base/list-groups", name: "List Groups", element: ListGroups },
-  { path: "/base/navs", name: "Navs", element: Navs },
-  { path: "/base/paginations", name: "Paginations", element: Paginations },
-  { path: "/base/placeholders", name: "Placeholders", element: Placeholders },
-  { path: "/base/popovers", name: "Popovers", element: Popovers },
-  { path: "/base/progress", name: "Progress", element: Progress },
-  { path: "/base/spinners", name: "Spinners", element: Spinners },
-  { path: "/base/tabs", name: "Tabs", element: Tabs },
-  { path: "/base/tables", name: "Tables", element: Tables },
-  { path: "/base/tooltips", name: "Tooltips", element: Tooltips },
-  { path: "/buttons", name: "Buttons", element: Buttons, exact: true },
-  { path: "/buttons/buttons", name: "Buttons", element: Buttons },
-  { path: "/buttons/dropdowns", name: "Dropdowns", element: Dropdowns },
   {
     path: "/incidents/create",
     name: "Create Incident",
