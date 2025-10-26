63228deb9a4bafb906d512973f19ea308f84a9ef
Jonas <mail@jonasgoebel.net>
2025-10-26 10:21:22 +0100

    set api url by env var

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 import autoprefixer from "autoprefixer";
 
-export default defineConfig(() => {
+export default defineConfig(({mode}) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
